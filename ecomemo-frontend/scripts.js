929fd3c9ed627c7c1b0ed7899241d0d58cfd74f6
moliver4 <tifftang22@gmail.com>
2019-12-30 16:27:21 -0800

    save modal in progress

--- ecomemo-frontend/scripts.js
+++ ecomemo-frontend/scripts.js
@@ -1,22 +1,26 @@
 window.addEventListener('DOMContentLoaded', e => {
 
-    const gameContainer = document.querySelector("#game-container")
     const gameClock = document.querySelector("#game-clock")
     const loginForm = document.querySelector("#login-form")
     const cards = document.querySelectorAll('.memory-card');
     const logout = document.querySelector("#logout")
     const startButton = document.querySelector("#start")
     const stopButton = document.querySelector("#stop")
+    const saveModal = document.querySelector("#save-game-modal")
+    
 
     cards.forEach(card => card.addEventListener('click', flipCard));
     shuffle()
+
 
     let username;
     let pairs = 0;
     let clockCounter = 0;
     let signedIn = false;
-    var timer;
+    let timer;
     let hasFlippedCard = false;
+    let officialSeconds = 0;
+
     let lockBoard = true; //board is locked until login
     let firstCard, secondCard;
 
@@ -37,9 +41,7 @@ window.addEventListener('DOMContentLoaded', e => {
             logout.style.display = "none";
         }
     }
-
-
-//game
+//button control
     function toggleDisable(button) {
         if (button.disabled) {
             button.removeAttribute("disabled");
@@ -48,11 +50,23 @@ window.addEventListener('DOMContentLoaded', e => {
         }
     }
 
+    function restartText(){
+        if (startButton.textContent === "Start Game") {
+            startButton.textContent = "Restart Game";
+        } 
+    }
+
+
+//game
+
     startButton.addEventListener('click', (e) => startGame())
     stopButton.addEventListener('click', (e) => stopGame())
 
     function startGame() {
-        toggleDisable(stopButton)
+        if (!stopButton.disabled) {
+            toggleDisable(stopButton)
+        }
+        officialSeconds = 0;
         restartText();
         lockBoard = false;
         pairs = 0;
@@ -66,18 +80,29 @@ window.addEventListener('DOMContentLoaded', e => {
 
     function stopGame() {
         clearInterval(timer)
+        let timerText = gameClock.innerHTML;
+        officialSeconds = calculateSeconds() //working, gives total seconds
+        const div = document.createElement('div');
+        const pg1 = document.createElement('p');
+        pg1.className = "final-time";
+        pg1.textContent = `Your Final Time: ${timerText}`
+        div.appendChild(pg1)
+        saveModal.firstElementChild.appendChild(div)
+        displayModal(saveModal)
+
+
+
     }
 
 
-    //toggles the Start/Restart Button
-    function restartText(){
-        if (startButton.textContent === "Start Game") {
-            startButton.textContent = "Restart Game";
-        } 
-    }
+
+
 //calculate score
-    function calculateScore() {
-
+    function calculateSeconds() {
+        let timeText = gameClock.innerHTML;
+        let timeArray = timeText.split(":");
+        let secondsCount = (parseInt(timeArray[0])*60) + parseInt(timeArray[1]);
+        return secondsCount;
     }
 
 //clock stuff
@@ -97,6 +122,16 @@ window.addEventListener('DOMContentLoaded', e => {
         return minutes + ':' + seconds;
     }
 
+//modal functionality
+    function displayModal(modal) {
+        modal.style.display = "block";
+        const closeSaveModal = document.querySelector("#close-save-game")
+        closeSaveModal.addEventListener('click', (e) => hideModal(saveModal))
+    }
+
+    function hideModal(modal) {
+        modal.style.display = "none";
+    }
 
 //card functionality
     
@@ -108,20 +143,20 @@ window.addEventListener('DOMContentLoaded', e => {
     };
 
     function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+        if (lockBoard) return;
+        if (this === firstCard) return;
 
-    this.classList.add('flip');
+        this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
 
-        return;
-    }
+            return;
+        }
 
-    secondCard = this;
-    checkForMatch();
+        secondCard = this;
+        checkForMatch();
     }
 
     function checkForMatch() {
@@ -132,12 +167,11 @@ window.addEventListener('DOMContentLoaded', e => {
 
     function disableCards() {
         pairs++
-        console.log(pairs)
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard);
 
         resetBoard();
-        if (pairs === 9) {
+        if (pairs >= 0) {
             toggleDisable(stopButton); //allow stop when all cards are flipped
         }
     }
